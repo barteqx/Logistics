f4eccd98631e28cd2ba99c1b3e1bc1d32100aa8b
barteqx <barteqx@gmail.com>
2015-04-01 07:27:09 +0100

    adding files

---
+++ Logistics.Domain.Model.User.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Logistics.Domain.Model.User.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
